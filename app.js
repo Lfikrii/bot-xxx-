325a3a96b53840288c8718dfb488a1ce73f360f1
Glitch (bot--xxx-) <none>
2021-01-11 05:27:08 +0000

    menambahkan //link dan mengganti let dengan var di variable prefix

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log('Pinging');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const Discord = require('discord.js')
 const client = new Discord.Client()
  
  
 client.login(process.env.TOKEN)
-let prefix = '//'
+var prefix = '//'
 const PREFIX = process.env.PREFIX 
  
 client.on('message', message => {
+  
+  if(message.content === '//link'){
+    message.channel.send('https://discord.gg/ZafN8qJJV8')
+  }
+    
   if(message.content.startsWith(`${prefix}ping`)) {
 const start = Date.now()
 message.channel.send(":ping_pong: Ping.. mencari ping..").then(message => {
@@ -21,8 +38,6 @@ message.edit(`:ping_pong: Pong! kamu membutuhkan waktu **${(end - start)}**ms!`)
     if(!text) return message.reply('Tolong beri aku kata kata!!!!!!')
     message.channel.send(text)
   }
-  
- 
- 
+   
  
 })
