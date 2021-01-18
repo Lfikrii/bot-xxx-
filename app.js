7a9e2b80d4bf4ebb156c8d602d5d41aff32c5caa
Glitch (bot--xxx-) <none>
2021-01-18 05:03:36 +0000

    menambahkan //help dan //sayd

--- app.js
+++ app.js
@@ -20,10 +20,16 @@ const PREFIX = process.env.PREFIX
  
 client.on('message', message => {
   
+  //cmd ambil link server
   if(message.content === '//link'){
     message.channel.send('https://discord.gg/ZafN8qJJV8')
   }
     
+  if(message.content === '//help'){
+    message.channel.send('```//link = untuk mengambil link server -xxx-``` ```//ping = pingpong``` ```//say = menyuruh bot mengatakan sesuatu``` ```//sayd = sama dengan say tapi menghapus perintah anda```')
+  }
+  
+  //cmd ping
   if(message.content.startsWith(`${prefix}ping`)) {
 const start = Date.now()
 message.channel.send(":ping_pong: Ping.. mencari ping..").then(message => {
@@ -33,11 +39,19 @@ message.edit(`:ping_pong: Pong! kamu membutuhkan waktu **${(end - start)}**ms!`)
 })
   }
   
+  //cmd say tapi delete
+  if(message.content.startsWith(`${prefix}sayd`)) {
+    message.delete()
+    var text = message.content.split(' ').slice(1).join(' ')
+    if(!text) return message.reply('Tolong beri aku kata kata!!!!!!')
+    message.channel.send(text)
+  }
+  
+  //cmd say biasa
   if(message.content.startsWith(`${prefix}say`)) {
     var text = message.content.split(' ').slice(1).join(' ')
     if(!text) return message.reply('Tolong beri aku kata kata!!!!!!')
     message.channel.send(text)
   }
-   
  
 })
